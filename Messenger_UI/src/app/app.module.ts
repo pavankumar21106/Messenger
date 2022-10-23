92f557e9d372a342406048d1a01d5530504e8a88
Pavan Kumar <pavankumar21106@gmail.com>
2022-10-23 22:54:01 +0530

    added send message ui

--- Messenger_UI/src/app/app.module.ts
+++ Messenger_UI/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './modules/login/login.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { InboxComponent } from './modules/inbox/inbox.component';
+import { ComposeComponent } from './modules/compose/compose.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    InboxComponent,
+    ComposeComponent
   ],
   imports: [
     BrowserModule,
