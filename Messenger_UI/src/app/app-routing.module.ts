ff023c2daf0cbde357f1888b5c27bf3653c84ead
Pavan Kumar <pavankumar21106@gmail.com>
2022-10-30 23:37:49 +0530

    refactoring

--- Messenger_UI/src/app/app-routing.module.ts
+++ Messenger_UI/src/app/app-routing.module.ts
@@ -6,9 +6,9 @@ import { LoginComponent } from './modules/login/login.component';
 import { NotFoundComponent } from './utility/not-found/not-found.component';
 
 const routes: Routes = [
+  { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'inbox', component: InboxComponent },
-  { path: 'compose', component: ComposeComponent },
   { path: 'compose', component: ComposeComponent },
   { path: '**', component: NotFoundComponent },
 ];
--- Messenger_UI/src/app/utility/not-found/not-found.component.ts
+++ Messenger_UI/src/app/utility/not-found/not-found.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-not-found',
   //remember:this is for svg as a template
-  // templateUrl: '../../../assets/icons/undraw_page_not_found_re_e9o6 (1).svg',
+  // templateUrl: '../../../assets/icons/page_not_found.svg',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss']
 })
