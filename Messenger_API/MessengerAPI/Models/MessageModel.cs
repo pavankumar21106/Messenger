4a6a501e8b708f7e2943d0fbb0e23da471499b04
Pavan Kumar <pavankumar21106@gmail.com>
2022-10-31 22:55:30 +0530

    use of record for models

--- Messenger_API/MessengerAPI/Models/MessageModel.cs
+++ Messenger_API/MessengerAPI/Models/MessageModel.cs
@@ -3,17 +3,18 @@ using Microsoft.AspNetCore.Html;
 
 namespace MessengerAPI.Models
 {
-    public class MessageModel
-    {
+    //remember: use record for model creation 
+    public record MessageModel
+    (
 
-        public string To { get; set; } = null!;
         //public string From { get; set; } = null!;
         //public string FromName { get; set; } = null!;
-        public string ToName { get; set; } = null!;
-        public string Subject { get; set; } = null!;
-        public string Body { get; set; } = null!;
-        public List<string>? Attachment { get; set; }
-    }
+        string To,
+        string ToName,
+        string Subject,
+        string Body,
+        List<string> Attachment
+    );
 
     public class BaseMessageResponseModel:  BaseModel
     {
