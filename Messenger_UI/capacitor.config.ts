a327bc634a2f2818c28a188c0d138b8fac8aee78
pavan <pavankumar84210@gmail.com>
2022-12-03 12:22:28 +0530

    converted it to angular 15 and created android app

---
+++ Messenger_UI/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.messengerui.app',
+  appName: 'messenger-uinpm install @capacitor/android -fnpx cap add android',
+  webDir: 'dist/messenger-ui',
+  bundledWebRuntime: false
+};
+
+export default config;
